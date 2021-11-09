75ab024896308400f28092c39abf22882b189091
Cyb3rowl <cybrowl@protonmail.com>
2021-11-09 11:58:40 -0800

    fix name of vars

---
+++ dfx.webpack.config.js
@@ -0,0 +1,41 @@
+const path = require("path")
+
+let localCanisters, prodCanisters, canisters
+
+function initCanisterIds() {
+  try {
+    localCanisters = require(path.resolve(
+      ".dfx",
+      "local",
+      "canister_ids.json"
+    ))
+  } catch (error) {
+    console.log('------------------------------------');
+    console.log("No local canister_ids.json found. Continuing production", error);
+    console.log('------------------------------------');
+  }
+  try {
+    prodCanisters = require(path.resolve("canister_ids.json"))
+  } catch (error) {
+    console.log('------------------------------------');
+    console.log("No production canister_ids.json found. Continuing with local");
+    console.log('------------------------------------');
+  }
+
+  const network = process.env.NODE_ENV === "production" ? "ic" : "local";
+
+  console.log('------------------------------------');
+  console.log(`initCanisterIds: network=${network}`);
+  console.log('------------------------------------');
+
+  canisters = network === "local" ? localCanisters : prodCanisters
+
+  for (const canister in canisters) {
+    process.env[`NEXT_PUBLIC_${canister.toUpperCase()}_CANISTER_ID`] =
+      canisters[canister][network]
+  }
+}
+
+module.exports = {
+  initCanisterIds: initCanisterIds,
+}
--- next.config.js
+++ next.config.js
@@ -1,5 +1,20 @@
-// next.config.js
+const DFXWebPackConfig = require("./dfx.webpack.config")
+DFXWebPackConfig.initCanisterIds()
+
+// const webpack = require("webpack")
+
+// // Make DFX_NETWORK available to Web Browser with default "local" if DFX_NETWORK is undefined
+// const EnvPlugin = new webpack.EnvironmentPlugin({
+//   DFX_NETWORK: "local",
+// })
 
 module.exports = {
-  swcMinify: true
+  swcMinify: true,
+  webpack: (config) => {
+    // Plugin
+    // config.plugins.push(EnvPlugin)
+
+    // Important: return the modified config
+    return config
+  },
 }
