a4fe401716ff07fdb3b3348c8ba8d5eb57ae4294
Cyb3rowl <cybrowl@protonmail.com>
2021-11-17 20:10:36 -0800

    update canisters to terminal based id

--- dfx.webpack.config.js
+++ dfx.webpack.config.js
@@ -14,6 +14,7 @@ function initCanisterIds() {
     console.log("No local canister_ids.json found. Continuing production");
     console.log('------------------------------------');
   }
+
   try {
     prodCanisters = require(path.resolve("canister_ids.json"))
   } catch (error) {
@@ -22,11 +23,13 @@ function initCanisterIds() {
     console.log('------------------------------------');
   }
 
-  const network = process.env.NODE_ENV === "production" ? "ic" : "local";
+  console.log('------------------------------------');
+  console.log("process.env.DFX_NETWORK: ", process.env.DFX_NETWORK);
+  console.log('------------------------------------');
 
-  console.log('------------------------------------');
-  console.log(`initCanisterIds: network=${network}`);
-  console.log('------------------------------------');
+  const network =
+    process.env.DFX_NETWORK ||
+    (process.env.NODE_ENV === "production" ? "ic" : "local");
 
   canisters = network === "local" ? localCanisters : prodCanisters
 
