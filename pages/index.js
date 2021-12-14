5b23f4b5d584076842e5e9d9496105d71648379c
cyberowl <cybrowl@protonmail.com>
2021-12-14 04:40:23 -0800

    update dfx version

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
       <Head>
-        <title>Create Next App</title>
+        <title>Apple</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
@@ -12,7 +12,7 @@ export default function Home() {
         <h1 className="text-6xl font-bold">
           Welcome to{' '}
           <a className="text-blue-600" href="https://nextjs.org">
-            Future!
+            Past!
           </a>
         </h1>
 
