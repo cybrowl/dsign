d891a7f902263a50e39ba6aaab2337b7de8bc214
cyberowl <cybrowl@protonmail.com>
2022-01-14 22:40:38 -0800

    clean: tests

---
+++ tests/actor.js
@@ -0,0 +1,31 @@
+const { Ed25519KeyIdentity } = require("@dfinity/identity");
+const { idlFactory } = require("../.dfx/local/canisters/profile/profile.did.test");
+
+const { HttpAgent, Actor } = require("@dfinity/agent");
+
+const HOST = "http://127.0.0.1:8000/";
+
+const getActor = async (canisterId) => {
+  const identity = Ed25519KeyIdentity.generate();
+
+  const agent = new HttpAgent({
+    host: HOST,
+    identity
+  });
+
+  agent.fetchRootKey().catch((err) => {
+    console.warn("Unable to fetch root key. Check to ensure that your local replica is running");
+    console.error(err);
+  });
+
+  const actor = Actor.createActor(idlFactory, {
+    agent: agent,
+    canisterId
+  });
+
+  return actor;
+};
+
+module.exports = {
+  getActor
+};
--- tests/profile.test.js
+++ tests/profile.test.js
@@ -1,56 +1,16 @@
-// const test = require("tape");
-// const fetch = require("node-fetch");
-// const { Ed25519KeyIdentity } = require("@dfinity/identity");
-// const {
-//   idlFactory,
-// } = require("../.dfx/local/canisters/profile/profile.did.test");
+const test = require("tape");
+const canisterIds = require("../.dfx/local/canister_ids.json");
+const fetch = require("node-fetch");
+const { getActor } = require("./actor");
 
-// // Actor/Canister Testing
+global.fetch = fetch;
 
-// global.fetch = fetch;
+test("Profile: ping()", async function (t) {
+  const canisterId = canisterIds.profile.local;
 
-// const { HttpAgent, Actor } = require("@dfinity/agent");
+  const profile = await getActor(canisterId);
+  const response = await profile.ping();
 
-// const HOST = "http://127.0.0.1:8000/";
-// const canisterId = "ryjl3-tyaaa-aaaaa-aaaba-cai";
-
-// const getActor = async (canisterId) => {
-//   const identity = Ed25519KeyIdentity.generate();
-
-//   const agent = new HttpAgent({
-//     host: HOST,
-//     identity,
-//   });
-
-//   agent.fetchRootKey().catch((err) => {
-//     console.warn(
-//       "Unable to fetch root key. Check to ensure that your local replica is running"
-//     );
-//     console.error(err);
-//   });
-
-//   const actor = Actor.createActor(idlFactory, {
-//     agent: agent,
-//     canisterId,
-//   });
-
-//   return actor;
-// };
-
-// test("Profile: ping()", async function (t) {
-//   const profile = await getActor(canisterId);
-//   const response = await profile.ping();
-
-//   t.equal(typeof response, "string");
-//   t.equal(response, "pong");
-// });
-
-// // test("Profile: canisterSize()", async function (t) {
-// //   const profile = await getActor(canisterId);
-// //   const response = await profile.canisterSize();
-
-// //   console.log("response=>", response);
-
-// //   t.equal(typeof response, "int");
-// //   t.equal(response, "pong");
-// // });
+  t.equal(typeof response, "string");
+  t.equal(response, "pong");
+});
