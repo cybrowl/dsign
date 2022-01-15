bc32838b77ba60757cc0b443f88c86dc52f71fcd
cyberowl <cybrowl@protonmail.com>
2022-01-15 04:38:51 -0800

    remove: watch dev to install vite

--- config/rollup.config.js
+++ config/rollup.config.js
@@ -5,37 +5,15 @@ import css from "rollup-plugin-css-only";
 import inject from "rollup-plugin-inject";
 import json from "@rollup/plugin-json";
 import sveltePreprocess from "svelte-preprocess";
-import livereload from "rollup-plugin-livereload";
 import resolve from "@rollup/plugin-node-resolve";
 import svelte from "rollup-plugin-svelte";
 const { generateCanisterAliases, getEnvironmentPath } = require("./dfx.config");
 
-const production = !process.env.ROLLUP_WATCH;
 const isDevelopment = process.env.DFX_NETWORK !== "ic";
+const isProduction = process.env.DFX_NETWORK === "ic";
 
 const aliases = generateCanisterAliases();
 const environment = getEnvironmentPath(isDevelopment);
-
-function serve() {
-  let server;
-
-  function toExit() {
-    if (server) server.kill(0);
-  }
-
-  return {
-    writeBundle() {
-      if (server) return;
-      server = require("child_process").spawn("npm", ["run", "start", "--", "--dev"], {
-        stdio: ["ignore", "inherit", "inherit"],
-        shell: true
-      });
-
-      process.on("SIGTERM", toExit);
-      process.on("exit", toExit);
-    }
-  };
-}
 
 const frontend = {
   input: "src/dsign_assets/main.js",
@@ -52,28 +30,18 @@ const frontend = {
         environment
       }
     }),
-
     svelte({
       compilerOptions: {
-        // enable run-time checks when not in production
-        dev: !production
+        dev: isDevelopment
       },
       preprocess: sveltePreprocess({
-        sourceMap: !production,
+        sourceMap: isDevelopment,
         postcss: {
           plugins: [require("tailwindcss")(), require("autoprefixer")()]
         }
       })
     }),
-    // we'll extract any component CSS out into
-    // a separate file - better for performance
     css({ output: "bundle.css" }),
-
-    // If you have external dependencies installed from
-    // npm, you'll most likely need these plugins. In
-    // some cases you'll need additional configuration -
-    // consult the documentation for details:
-    // https://github.com/rollup/plugins/tree/master/packages/commonjs
     resolve({
       preferBuiltins: false,
       browser: true,
@@ -84,18 +52,7 @@ const frontend = {
       Buffer: ["buffer", "Buffer"]
     }),
     json(),
-
-    // In dev mode, call `npm run start` once
-    // the bundle has been generated
-    !production && serve(),
-
-    // Watch the `public` directory and refresh the
-    // browser on changes when not in production
-    !production && livereload("public"),
-
-    // If we're building for production (npm run build
-    // instead of npm run dev), minify
-    production && terser()
+    isProduction && terser()
   ],
   watch: {
     clearScreen: false
