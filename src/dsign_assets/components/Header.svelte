687457f368837029de7bc3da6581b91193a183f7
cyberowl <cybrowl@protonmail.com>
2022-01-31 23:24:00 -0800

    fix: styling

--- src/dsign_assets/components/Header.svelte
+++ src/dsign_assets/components/Header.svelte
@@ -3,12 +3,12 @@
 </script>
 
 <div class="col-span-12 m-10">
-	<div class="flex justify-start">
-		<a href="#_" class="mr-5">DSign</a>
+	<div class="">
 		<nav>
-			<a href="#_" class="mr-5 font-medium hover:text-gray-900">Explore</a>
-			<a href="#_" class="mr-5 font-medium hover:text-gray-900">Projects</a>
-			<a href="#_" class="font-medium hover:text-gray-900">Favorites</a>
+			<a href="#_" class="mr-5 font-large uppercase">DSign</a>
+			<a href="#_" class="mr-5 font-medium hover:underline">Explore</a>
+			<a href="#_" class="mr-5 font-medium hover:underline">Projects</a>
+			<a href="#_" class="mr-5 font-medium hover:underline">Favorites</a>
 		</nav>
 	</div>
 	<div class="flex justify-end justify-items-end">
