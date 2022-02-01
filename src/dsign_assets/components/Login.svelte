28099bbaef7fdb6736e197c6d3245ecab6d31431
cyberowl <cybrowl@protonmail.com>
2022-01-31 18:30:10 -0800

    fix: log in button

--- src/dsign_assets/components/Login.svelte
+++ src/dsign_assets/components/Login.svelte
@@ -41,7 +41,7 @@
 	{:else}
 		<button
 			class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-			on:click={login}>Login</button
+			on:click={login}>Log In</button
 		>
 	{/if}
 </span>
