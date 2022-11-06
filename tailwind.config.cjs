5aeee1f150b64f2ff548376450a351ab2c8b0e0d
cyberowl <cybrowl@protonmail.com>
2022-11-06 01:37:00 -0700

    fix: missed one warning

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,6 +1,5 @@
 module.exports = {
-	content: ['./src/dsign_assets/**/*.{html,js,cjs, svelte}'],
-	purge: ['./src/**/*.html', './src/**/*.svelte'],
+	content: ['./src/dsign_assets/**/*.{html,js,cjs,svelte}', './src/**/*.html', './src/**/*.svelte'],
 	darkMode: 'class', // or 'media' or 'class'
 	theme: {
 		extend: {
