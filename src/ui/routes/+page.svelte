b9ce109853b72c9cd513c8e4bea211e724c50e2e
cyberowl <cybrowl@protonmail.com>
2024-02-22 01:26:14 -0800

    add: splash screen while I refactor

--- src/ui/routes/+page.svelte
+++ src/ui/routes/+page.svelte
@@ -3,17 +3,17 @@
 	import { onMount } from 'svelte';
 	import get from 'lodash/get';
 
-	import Login from '../components/Login.svelte';
-	import { Notification, PageNavigation, ProjectCard } from 'dsign-components';
-	import AccountSettingsModal from '$modals_ref/AccountSettingsModal.svelte';
+	// import Login from '../components/Login.svelte';
+	// import { Notification, PageNavigation, ProjectCard } from 'dsign-components';
+	// import AccountSettingsModal from '$modals_ref/AccountSettingsModal.svelte';
 
-	import { actor_explore } from '$stores_ref/actors.js';
+	// import { actor_explore } from '$stores_ref/actors.js';
 	import { disable_project_store_reset } from '$stores_ref/page_state';
 	import { explore_store, projects_update } from '$stores_ref/fetch_store.js';
-	import { modal_visible } from '$stores_ref/modal';
-	import { notification_visible, notification } from '$stores_ref/notification';
-	import { page_navigation } from '$stores_ref/page_navigation';
-	import { init_auth } from '$stores_ref/auth_client';
+	// import { modal_visible } from '$stores_ref/modal';
+	// import { notification_visible, notification } from '$stores_ref/notification';
+	// import { page_navigation } from '$stores_ref/page_navigation';
+	// import { init_auth } from '$stores_ref/auth_client';
 
 	disable_project_store_reset.set(true);
 
@@ -58,10 +58,20 @@
 <!-- Explore -->
 <svelte:head>
 	<title>DSign</title>
+	<script
+		type="module"
+		src="https://unpkg.com/@splinetool/viewer@1.0.53/build/spline-viewer.js"
+	></script>
 </svelte:head>
 
 <main class="grid_layout">
-	<div class="navigation_main_layout">
+	<header class="header_title">New Architecture Coming Soon</header>
+	<div class="projects_layout">
+		<spline-viewer url="https://prod.spline.design/1hpqInIIL-bNm99m/scene.splinecode"
+		></spline-viewer>
+	</div>
+
+	<!-- <div class="navigation_main_layout">
 		<PageNavigation
 			navigationItems={$page_navigation.navigationItems}
 			on:home={() => {
@@ -70,24 +80,115 @@
 		>
 			<Login />
 		</PageNavigation>
-	</div>
+	</div> -->
+
+	<!-- <div class="navigation_main_layout">
+		<PageNavigation
+			navigationItems={$page_navigation.navigationItems}
+			on:home={() => {
+				goto('/');
+			}}
+		>
+			<Login />
+		</PageNavigation>
+	</div> -->
+	<!-- <div class="navigation_main_layout">
+		<PageNavigation
+			navigationItems={$page_navigation.navigationItems}
+			on:home={() => {
+				goto('/');
+			}}
+		>
+			<Login />
+		</PageNavigation>
+	</div> -->
+	<!-- <div class="navigation_main_layout">
+		<PageNavigation
+			navigationItems={$page_navigation.navigationItems}
+			on:home={() => {
+				goto('/');
+			}}
+		>
+			<Login />
+		</PageNavigation>
+	</div> -->
+	<!-- <div class="navigation_main_layout">
+		<PageNavigation
+			navigationItems={$page_navigation.navigationItems}
+			on:home={() => {
+				goto('/');
+			}}
+		>
+			<Login />
+		</PageNavigation>
+	</div> -->
+	<!-- <div class="navigation_main_layout">
+		<PageNavigation
+			navigationItems={$page_navigation.navigationItems}
+			on:home={() => {
+				goto('/');
+			}}
+		>
+			<Login />
+		</PageNavigation>
+	</div> -->
+	<!-- <div class="navigation_main_layout">
+		<PageNavigation
+			navigationItems={$page_navigation.navigationItems}
+			on:home={() => {
+				goto('/');
+			}}
+		>
+			<Login />
+		</PageNavigation>
+	</div> -->
+	<!-- <div class="navigation_main_layout">
+		<PageNavigation
+			navigationItems={$page_navigation.navigationItems}
+			on:home={() => {
+				goto('/');
+			}}
+		>
+			<Login />
+		</PageNavigation>
+	</div> -->
+	<!-- <div class="navigation_main_layout">
+		<PageNavigation
+			navigationItems={$page_navigation.navigationItems}
+			on:home={() => {
+				goto('/');
+			}}
+		>
+			<Login />
+		</PageNavigation>
+	</div> -->
+	<!-- <div class="navigation_main_layout">
+		<PageNavigation
+			navigationItems={$page_navigation.navigationItems}
+			on:home={() => {
+				goto('/');
+			}}
+		>
+			<Login />
+		</PageNavigation>
+	</div> -->
 
 	<!-- AccountSettingsModal -->
-	{#if $modal_visible.account_settings}
+	<!-- {#if $modal_visible.account_settings}
 		<AccountSettingsModal />
-	{/if}
+	{/if} -->
 
 	<!-- Notification -->
-	{#if $notification_visible.auth_error}
+	<!-- {#if $notification_visible.auth_error}
 		<div class="notification_layout">
 			<Notification is_visible={$notification_visible.auth_error} hide_delay_sec={2000}>
 				<p>{$notification.message}</p>
 			</Notification>
 		</div>
-	{/if}
+	{/if} -->
 
 	<!-- Projects -->
-	{#if $explore_store.projects.length > 0}
+	<!-- {#if $explore_store.projects.length > 0}
 		<div class="projects_layout">
 			{#each $explore_store.projects as project (project.id)}
 				<ProjectCard
@@ -100,7 +201,7 @@
 				/>
 			{/each}
 		</div>
-	{/if}
+	{/if} -->
 </main>
 
 <!-- Mobile Not Supported -->
@@ -111,7 +212,7 @@
 	</div>
 {/if}
 
-<style lang="postcss">
+<!-- <style lang="postcss">
 	.grid_layout {
 		@apply hidden lg:grid grid-cols-12 gap-y-2 relative mx-12 2xl:mx-60;
 	}
@@ -126,5 +227,31 @@
 	}
 	.not_supported {
 		@apply lg:hidden flex flex-col items-center justify-center w-screen h-screen text-white text-4xl;
+	}
+</style> -->
+
+<style>
+	.projects_layout {
+		width: 100vw;
+		height: 100vh;
+		position: relative; /* This ensures that the header can be absolutely positioned within */
+	}
+
+	.header_title {
+		position: absolute; /* Positioning the header absolutely to stay at the top */
+		top: 0; /* Align to the top of the projects_layout */
+		width: 100%; /* Span the full width */
+		text-align: center; /* Center the text */
+		background-color: rgba(0, 0, 0, 0.7); /* Semi-transparent background */
+		color: white; /* Text color */
+		padding: 10px 0; /* Some padding */
+		z-index: 10; /* Ensure it's above the spline viewer */
+		font-size: 24px; /* Adjust font size as needed */
+	}
+
+	spline-viewer {
+		width: 100%;
+		height: 100%;
+		display: block; /* Ensure it's not inline */
 	}
 </style>
