f345cce3824a3b4d13afec708418defd019671af
cyberowl <cybrowl@protonmail.com>
2024-03-08 23:53:04 -0800

    remove: redundant store and local storage functions
    This commit removes unnecessary code from the fetch_store.js and local_storage.js files. The changes include:
    - Deletion of fetch_store.js, which contained several exported stores and related update functions that are no longer needed.
    - Removal of unused export functions in local_storage.js, including individual remove item function and deprecated persisted data items.
    - Simplification of the 'local_storage_remove_all' function to only remove 'my_profile' item.

--- src/ui/store/local_storage.js
+++ src/ui/store/local_storage.js
@@ -1,12 +1,6 @@
 import { persisted } from '../utils/local_storage';
 import { browser } from '$app/environment';
 
-export function local_storage_remove(key) {
-	if (!browser) return;
-
-	localStorage.removeItem(key);
-}
-
 const empty_profile = {
 	created: 0n,
 	username: '',
@@ -19,30 +13,10 @@ const empty_profile = {
 	avatar: { id: '', url: '', canister_id: '' }
 };
 
-export const ls_profile = persisted('profile', empty_profile);
 export const ls_my_profile = persisted('my_profile', empty_profile);
-
-// TODO: deprecate
-export const local_storage_favorites = persisted('favorites', { all_favorites_count: 1 });
-export const local_storage_projects = persisted('projects', { all_projects_count: 1 });
-export const local_storage_snaps = persisted('snaps', { all_snaps_count: 1 });
-
-export const local_snap_creation_design_file = persisted('design_file', {
-	file_name: '',
-	file_type: '',
-	chunk_ids: []
-});
-
-export const local_storage_profile = persisted('profile', {
-	avatar_url: '',
-	banner_url: '',
-	username: ''
-});
 
 export function local_storage_remove_all() {
 	if (!browser) return;
 
-	localStorage.removeItem('snaps');
-	localStorage.removeItem('favorites');
-	localStorage.removeItem('projects');
+	localStorage.removeItem('my_profile');
 }
