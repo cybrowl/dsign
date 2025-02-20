48dc663647a495adcce576f1d469a7119dda5ceb
cyberowl <cybrowl@protonmail.com>
2025-02-20 14:23:05 -0800

    fix: scripts for deployment

--- scripts/deploy_local.sh
+++ scripts/deploy_local.sh
@@ -4,41 +4,41 @@ echo "env: dev"
 cp ./src/env/env_local.mo ./src/env/env.mo
 
 # Cycles
-dfx ledger fabricate-cycles --all
+dfx ledger fabricate-cycles --all --network local
 
 # II
-dfx deploy internet_identity
+dfx deploy internet_identity --network local
 
 # Username Registry
-dfx deploy username_registry
-export USERNAME_REGISTRY_CANISTER_ID=$(dfx canister id username_registry)
+dfx deploy username_registry --network local
+export USERNAME_REGISTRY_CANISTER_ID=$(dfx canister --network local id username_registry)
 
 # Explore
-dfx deploy explore
+dfx deploy explore --network local
 
 # M-O
-dfx deploy mo
+dfx deploy mo --network local
 
 # Creator
-dfx deploy creator --argument='(principal "'${USERNAME_REGISTRY_CANISTER_ID}'")'
+dfx deploy creator --network local --argument='(principal "'${USERNAME_REGISTRY_CANISTER_ID}'")'
 
 # File Storage
-dfx deploy file_storage --argument='(false, "8080", 10)'
+dfx deploy file_storage --network local --argument='(false, "8080", 10)'
 
 # File Scaling
-dfx deploy file_scaling_manager --argument='(false, "8080", 10)'
+dfx deploy file_scaling_manager --network local --argument='(false, "8080", 10)'
 
 # Logger
-dfx deploy logger
+dfx deploy logger --network local
 
 ### Initialize Canisters
 # Must be called before anything else
-dfx canister call explore init '(principal "'${USERNAME_REGISTRY_CANISTER_ID}'")' 
-dfx canister call mo init '(principal "'${USERNAME_REGISTRY_CANISTER_ID}'")' 
+dfx canister --network local call explore init '(principal "'${USERNAME_REGISTRY_CANISTER_ID}'")'
+dfx canister --network local call mo init '(principal "'${USERNAME_REGISTRY_CANISTER_ID}'")'
 
-dfx canister call username_registry init
-dfx canister call file_scaling_manager init
-dfx canister call logger init
+dfx canister --network local call username_registry init
+dfx canister --network local call file_scaling_manager init
+dfx canister --network local call logger init
 
 ### Wait before deploying children
 echo "Waiting for canisters to stabilize..."
--- scripts/deploy_prod.sh
+++ scripts/deploy_prod.sh
@@ -12,53 +12,52 @@ if [ "$DEPLOY_ENV" == "prod" ]; then
     cp ./src/env/env_ic.mo ./src/env/env.mo
 
     DEPLOY_NETWORK="--network ic"
-    DEPLOY_WALLET="--wallet=l2eht-qyaaa-aaaag-aaarq-cai"
+    DEPLOY_IDENTITY="--identity dsign"
 
 elif [ "$DEPLOY_ENV" == "staging" ]; then
     echo "------------------------------"
     echo "env: staging"
     echo "------------------------------"
-
     cp ./src/env/env_staging.mo ./src/env/env.mo
 
     DEPLOY_NETWORK="--network staging"
-    DEPLOY_WALLET="--wallet=l2eht-qyaaa-aaaag-aaarq-cai"
+    DEPLOY_IDENTITY="--identity dsign"
 
 else
     echo "Unsupported deployment environment. Only 'prod' and 'staging' are supported."
     exit 1
 fi
 
-# Username Registry
-dfx deploy ${DEPLOY_NETWORK} ${DEPLOY_WALLET} username_registry
+#Username Registry
+dfx deploy ${DEPLOY_NETWORK} ${DEPLOY_IDENTITY} username_registry
 export USERNAME_REGISTRY_CANISTER_ID=$(dfx canister ${DEPLOY_NETWORK} id username_registry)
 
 # Explore
-dfx deploy ${DEPLOY_NETWORK} ${DEPLOY_WALLET} explore
+dfx deploy ${DEPLOY_NETWORK} ${DEPLOY_IDENTITY} explore
 
 # M-O
-dfx deploy ${DEPLOY_NETWORK} ${DEPLOY_WALLET} mo
+dfx deploy ${DEPLOY_NETWORK} ${DEPLOY_IDENTITY} mo
 
 # Creator
-dfx deploy ${DEPLOY_NETWORK} ${DEPLOY_WALLET} creator --argument='(principal "'${USERNAME_REGISTRY_CANISTER_ID}'")'
+dfx deploy ${DEPLOY_NETWORK} ${DEPLOY_IDENTITY} creator --argument='(principal "'${USERNAME_REGISTRY_CANISTER_ID}'")'
 
 # File Storage
-dfx deploy ${DEPLOY_NETWORK} ${DEPLOY_WALLET} file_storage --argument='(true, "8080", 1500)'
+dfx deploy ${DEPLOY_NETWORK} ${DEPLOY_IDENTITY} file_storage --argument='(true, "8080", 1500)'
 
 # File Scaling
-dfx deploy ${DEPLOY_NETWORK} ${DEPLOY_WALLET} file_scaling_manager --argument='(true, "8080", 1500)'
+dfx deploy ${DEPLOY_NETWORK} ${DEPLOY_IDENTITY} file_scaling_manager --argument='(true, "8080", 1500)'
 
 # Logger
-dfx deploy ${DEPLOY_NETWORK} ${DEPLOY_WALLET} logger
+dfx deploy ${DEPLOY_NETWORK} ${DEPLOY_IDENTITY} logger
 
 ### Initialize Canisters
 # Must be called before anything else
-dfx canister ${DEPLOY_NETWORK} ${DEPLOY_WALLET} call explore init '(principal "'${USERNAME_REGISTRY_CANISTER_ID}'")' 
-dfx canister ${DEPLOY_NETWORK} ${DEPLOY_WALLET} call mo init '(principal "'${USERNAME_REGISTRY_CANISTER_ID}'")' 
+dfx canister ${DEPLOY_NETWORK} ${DEPLOY_IDENTITY} call explore init '(principal "'${USERNAME_REGISTRY_CANISTER_ID}'")'
+dfx canister ${DEPLOY_NETWORK} ${DEPLOY_IDENTITY} call mo init '(principal "'${USERNAME_REGISTRY_CANISTER_ID}'")'
 
-dfx canister ${DEPLOY_NETWORK} ${DEPLOY_WALLET} call username_registry init 
-dfx canister ${DEPLOY_NETWORK} ${DEPLOY_WALLET} call file_scaling_manager init  
-dfx canister ${DEPLOY_NETWORK} ${DEPLOY_WALLET} call logger init  
+dfx canister ${DEPLOY_NETWORK} ${DEPLOY_IDENTITY} call username_registry init
+dfx canister ${DEPLOY_NETWORK} ${DEPLOY_IDENTITY} call file_scaling_manager init
+dfx canister ${DEPLOY_NETWORK} ${DEPLOY_IDENTITY} call logger init
 
 ### Wait before deploying children
 echo "Waiting for canisters to stabilize..."
@@ -68,4 +67,4 @@ sleep 20
 npm run deploy:children
 
 # UI
-dfx deploy ${DEPLOY_NETWORK} ${DEPLOY_WALLET} ui
+dfx deploy ${DEPLOY_NETWORK} ${DEPLOY_IDENTITY} ui
